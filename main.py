73fea728fce019b721fa9b0998aa276f6a7c50ad
david <dale.david@mail.ru>
2021-03-04 10:07:49 +0300

    add mongo

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from dialogic.dialog_connector import DialogConnector
 from dialogic.server.flask_server import FlaskServer
+from dialogic.storage.message_logging import MongoMessageLogger
 
 from dm import make_dm
 import scenarios  # noqa
 
 dm = make_dm()
-connector = DialogConnector(dialog_manager=dm)
+connector = DialogConnector(
+    dialog_manager=dm,
+    log_storage=MongoMessageLogger(),  # requires MONGODB_URI env variable
+)
 server = FlaskServer(connector=connector)
 
 handler = connector.serverless_alice_handler
